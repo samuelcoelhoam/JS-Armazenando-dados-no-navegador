84bb9add49c4dd2c54148d7609a4677f0e92c969
infoboyz <jorge12sam@gmail.com>
2022-11-09 12:25:39 -0400

    Consertando erros

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-itens.forEach( (element) => {
-    criaElemento(element);
+itens.forEach( (elemento) => {
+    criaElemento(elemento);
 });
 
 form.addEventListener("submit", (evento) => {
@@ -11,20 +11,21 @@ form.addEventListener("submit", (evento) => {
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-    const existe = itens.find(elemento => elemento.name === nome.value);
+    const existe = itens.find(elemento => elemento.nome === nome.value);
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
     if(existe) {
-        itemAtual.id = existe.id
+        itemAtual.id = existe.id;
         atualizaElemento(itemAtual);
         itens[existe.id] = itemAtual;
+
     } else {
-        itemAtual.id = itens.lenght;
-        itens.push(itemAtual);
+        itemAtual.id = itens.length;
         criaElemento(itemAtual);
+        itens.push(itemAtual);
     }
 
     localStorage.setItem("itens",  JSON.stringify(itens));
